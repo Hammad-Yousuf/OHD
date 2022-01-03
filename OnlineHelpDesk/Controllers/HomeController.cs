0171563f64ed8d6a01565b59f2db94f1763a6c9f
M. Hammad Yousuf <74192966+Hammad-Yousuf@users.noreply.github.com>
2022-01-03 18:46:33 +0500

    Update

--- OnlineHelpDesk/Controllers/HomeController.cs
+++ OnlineHelpDesk/Controllers/HomeController.cs
@@ -8,6 +8,8 @@ namespace OnlineHelpDesk.Controllers
 {
     public class HomeController : Controller
     {
+        OHDEntities db = new OHDEntities();
+
         // GET: Home
         public ActionResult Index()
         {
@@ -25,5 +27,28 @@ namespace OnlineHelpDesk.Controllers
         {
             return View();
         }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        
+        public ActionResult SignUp(FormCollection fc)
+        {
+            User data = new User();
+            data.User_Name = Request.Form["new_name"];
+            data.User_Email = Request.Form["new_email"];
+            data.User_Password = Request.Form["password"];
+            data.User_Phone = Request.Form["new_phone"];
+            data.User_Type = Request.Form["new_user_type"];
+            db.Users.Add(data);
+            db.SaveChanges();
+            return RedirectToAction("Login");
+
+            
+        }
+
     }
 }
